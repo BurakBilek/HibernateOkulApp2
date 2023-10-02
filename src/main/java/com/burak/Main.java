ae75cad7e23d49bf246d7461f428378cd18087b6
burak <burakbilek96@hotmail.com>
2023-10-03 00:49:59 +0300

    Odev

--- src/main/java/com/burak/Main.java
+++ src/main/java/com/burak/Main.java
@@ -1,7 +1,9 @@
 package com.burak;
 
 
+import com.burak.controller.OgrenciController;
 import com.burak.repository.entity.*;
+import com.burak.repository.hql.OgrenciDao;
 import com.burak.util.HibernateUtility;
 import com.burak.util.MyFactoryRepository;
 import org.hibernate.Session;
@@ -12,29 +14,95 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-        Session session ;
-        Transaction transaction ;
-        session= HibernateUtility.getSessionFactory().openSession();
-        transaction=session.beginTransaction();
-        MyFactoryRepository<Ogrenci,Long> ogrenciRepository= new MyFactoryRepository<>(new Ogrenci());
-        ogrenciRepository.findAll();
-        Long id=1L;
-        System.out.println(ogrenciRepository.findById(id));
-        System.out.println("Diger Asama");
+//        Session session = HibernateUtility.getSessionFactory().openSession();
+//        Transaction transaction = session.beginTransaction();
+//        Ogretmen ogretmen = Ogretmen.builder()
+//                .kisiselBilgiler(KisiselBilgiler.builder()
+//                        .isim("Alperen")
+//                        .soyisim("Ikinci")
+//                        .tcKimlikNo("1564789")
+//                        .build())
+//                .brans(EBrans.FIZIK)
+////                .iseBaslamaTarihi(LocalDate.of(2022,01,15))
+//                .build();
+//        session.save(ogretmen);
+//        Ogrenci ogrenci = Ogrenci.builder()
+//                .kisiselBilgiler(KisiselBilgiler.builder()
+//                        .isim("Banu")
+//                        .soyisim("Yilmaz")
+//                        .tcKimlikNo("12345")
+//                        .build())
+//                .build();
+//        Ogrenci ogrenci2 = Ogrenci.builder()
+//                .kisiselBilgiler(KisiselBilgiler.builder()
+//                        .isim("Ahmet")
+//                        .soyisim("Kaya")
+//                        .tcKimlikNo("123456")
+//                        .build())
+//
+//                .dogumTarihi(LocalDate.of(1990,10,10))
+//                .build();
+//        Sinif sinif = Sinif.builder()
+//                .sinifAdi("Kelebekler Sinifi")
+//                .ogretmenId(ogretmen.getId())
+//                .ogrenciler(List.of(
+//                        ogrenci.getKisiselBilgiler().getIsim(),
+//                        ogrenci2.getKisiselBilgiler().getIsim()
+//                ))
+//                .build();
+//
+//        session.save(ogrenci);
+//        session.save(ogrenci2);
+//        session.save(sinif);
+//        transaction.commit();
+//        session.close();
+//        OgrenciCriteria ogrenciCriteria = new OgrenciCriteria();
+//        Ogrenci ogrenci = ogrenciCriteria.findById(1L);
+//        List<Ogrenci> ogrenciList = ogrenciCriteria.findAll();
+//        System.out.println("Tekli ogrenci (findById : " + ogrenci);
+//        System.out.println("######################################");
+//        ogrenciList.forEach(x -> System.out.println(x));
 
-        MyFactoryRepository<Ogretmen,Long>ogretmenList= new MyFactoryRepository<>(new Ogretmen());
-        ogretmenList.findAll();
-        System.out.println(ogretmenList.findById(id));
+//        OgretmenCriteria ogretmenCriteria = new OgretmenCriteria();
+//        Ogretmen ogretmen = ogretmenCriteria.findById(1L);
+//        List<Ogretmen> ogretmenList = ogretmenCriteria.findAll();
+//        System.out.println("Tekli ogretmen (findById : " + ogretmen);
+//        System.out.println("######################################");
+//        ogretmenList.forEach(x -> System.out.println(x));
 
-        System.out.println("Diger Asama");
+        Ogrenci ogrenci = Ogrenci.builder()
+                .kisiselBilgiler(KisiselBilgiler.builder()
+                        .isim("Ali")
+                        .soyisim("Dogan")
+                        .tcKimlik("1234789")
+                        .build())
+                .dogumTarihi(LocalDate.of(1995,3,19))
+                .build();
+        OgrenciController ogrenciController = new OgrenciController();
+        ogrenciController.save(ogrenci);
 
-        MyFactoryRepository<Sinif,Long>sinifList= new MyFactoryRepository<>(new Sinif());
-        sinifList.findAll();
-        System.out.println(sinifList.findById(id));
+//        session.save(ogrenci);
+//        transaction.commit();
+//        session.close();
+
+
+
+//        OgrenciDao ogrenciDao = new OgrenciDao();
+//        List<Ogrenci> ogrenciList = ogrenciDao.findAll2();
+//        ogrenciList.stream().forEach(x-> System.out.println(x));
+//        System.out.println(ogrenciDao.findById2(1L));
+
     }
 }
+     /*   OgretmenCriteria ogretmenCriteria = new OgretmenCriteria();
+        Ogretmen ogretmen = ogretmenCriteria.findById(1L);
+        List<Ogretmen> ogretmenList = ogretmenCriteria.findAll();
+        System.out.println("Tekli ogretmen (findById : " + ogretmen);
+        System.out.println("######################################");
+        ogretmenList.forEach(x -> System.out.println(x));
+*/
+
 
     /*
     public static void main(String[] args) {
